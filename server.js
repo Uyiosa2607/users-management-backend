b7fe7d82a037f077c6a6b0512e10f3ff281797f0
Caveman707 <[johnsean6268@gmail.com]>
2023-11-21 00:31:09 +0100

    production

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api", router);
 
-app.use(express.static('public'));
-
 app.get("/", (req, res) => {
 
     res.status(200).send("Backend Server is up and running")
