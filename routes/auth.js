33407e1cf2cab847076f474a4926b6f879b753e4
Caveman707 <[johnsean6268@gmail.com]>
2023-11-21 14:07:58 +0100

    production

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.post("/login",async (req, res) => {
 
   const {email, password } = req.body;
 
-  const user = await User.findOne({ email }).select("-password");
+  const user = await User.findOne({ email });
 
   if(user) {
 
@@ -52,14 +52,15 @@ router.post("/login",async (req, res) => {
           expiresIn: "15m",
         });
 
+        const { password, ...others } = user._doc;
 
         res.cookie("token", accessToken, { maxAge: 20 * 60 * 1000,
           sameSite: "none",
           secure: true,
-          httpOnly:true
+          httpOnly: true,
          });
 
-        res.status(200).json({ user, accessToken });
+        res.status(200).json({ ...others, accessToken });
     
       } else {
 
